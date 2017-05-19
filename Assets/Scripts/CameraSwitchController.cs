3aa4a8a81c13f218196ab391fd580a6a76761ec8
Jian-Bin Liu <billyljb@gmail.com>
2017-05-20 01:04:45 +0800

    update camera multi-view

---
+++ Assets/Scripts/CameraSwitchController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitchController : MonoBehaviour {
+	public Camera topCamera;
+	public Camera MainCamera;
+
+	private float pushTime = 0.31f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Tab)  && pushTime > 0.3f) {
+			pushTime = 0;
+			if (topCamera.depth != 1) {
+				topCamera.depth = 1;
+			} else {
+				topCamera.depth = -1;
+			}
+		}
+		pushTime += Time.deltaTime;
+		
+	}
+}
