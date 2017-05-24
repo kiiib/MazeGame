8fbed09d3ccdde59ac62f6236ec35b01cb090a1c
Jian-Bin Liu <billyljb@gmail.com>
2017-05-24 23:06:22 +0800

    update second control way of player

---
+++ Assets/Scripts/Player2Controller.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player2Controller : MonoBehaviour
+{
+
+    public float MoveSpeed;
+    float currentSpeed = 0;
+
+    public Rigidbody rigidBody;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            var vector = this.transform.forward * MoveSpeed;
+            this.gameObject.transform.position += vector;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            var vector = this.transform.forward * MoveSpeed;
+            this.gameObject.transform.position -= vector;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.gameObject.transform.Rotate(new Vector3(0, -1, 0));
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.gameObject.transform.Rotate(new Vector3(0, 1, 0));
+        }
+
+    }
+
+
+}
